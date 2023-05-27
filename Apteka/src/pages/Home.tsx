54fab395df4f2ade9a5b3658654d8022e49c17d3
mateusz2626 <129773593+mateusz2626@users.noreply.github.com>
2023-05-28 00:42:22 +0200

    Delete Apteka directory

--- Apteka/src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-import { useState } from "react";
-
-export function Home() {
-  return <h1>Strona Główna</h1>;
-}
--- Apteka/src/utilities/formatCurrency.ts
+++
@@ -1,9 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat('pl', {
-    currency: "PLN",
-    style: "currency",
-    currencyDisplay: 'narrowSymbol'
-  })
-  
-  export function formatCurrency(number: number) {
-    return CURRENCY_FORMATTER.format(number)
-  }
